c03b1cd691075e497797de64e3eae8318a15bc3d
Sarah Wu <wusarah9@gmail.com>
2021-04-12 16:34:34 -0700

    fixed display of form

--- public/examples.js
+++ public/examples.js
@@ -1,27 +1,28 @@
 let pastbtn = document.getElementById('past-btn');
 let pastform = document.getElementById('past-form');
-
-pastbtn.onclick = function(){
-    pastbtn.style.display = 'none';
-    pastform.style.display = 'block';
-};
+let pastactsubmit = document.getElementById('past-act-submit');
 
 let futurebtn = document.getElementById('future-btn');
 let futureform = document.getElementById('future-form');
+let pastsubmit = document.getElementById('past-submit');
+let futuresubmit = document.getElementById('future-submit');
+
+pastbtn.onclick = function(){
+    pastbtn.style.display = 'none';
+    pastform.style.display = 'flex';
+    pastsubmit.style.display = 'block'
+    pastactsubmit.style.display = 'none'
+};
 
 futurebtn.onclick = function(){
     futurebtn.style.display = 'none';
     futureform.style.display = 'block';
 };
 
-let pastsubmit = document.getElementById('past-submit');
-let futuresubmit = document.getElementById('future-submit');
-
 pastsubmit.onclick = function(){
     pastform.style.display = 'none';
     pastsubmit.style.display = 'none';
-    let pastactsubmit = document.getElementById('past-act-submit');
-    pastactsubmit.textContent = "hello!";
+    pastactsubmit.textContent = "hello!\n";
     pastactsubmit.style.display = 'block';
     pastbtn.style.display = 'block';
 }
