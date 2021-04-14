d793c2cd5453ae1acfd0cbdcebb4c924e34eb4c5
Sarah Wu <wusarah9@gmail.com>
2021-04-13 17:48:27 -0700

    added more js

--- public/examples.js
+++ public/examples.js
@@ -1,11 +1,14 @@
-let pastbtn = document.getElementById('past-btn');
-let pastform = document.getElementById('past-form');
-let pastactsubmit = document.getElementById('past-act-submit');
+const pastbtn = document.getElementById('past-btn');
+const pastform = document.getElementById('past-form');
 
-let futurebtn = document.getElementById('future-btn');
-let futureform = document.getElementById('future-form');
-let pastsubmit = document.getElementById('past-submit');
-let futuresubmit = document.getElementById('future-submit');
+const futurebtn = document.getElementById('future-btn');
+const futureform = document.getElementById('future-form');
+
+const pastsubmit = document.getElementById('past-submit');
+const futuresubmit = document.getElementById('future-submit');
+
+const pastactsubmit = document.getElementById('past-act-submit');
+const futureactsubmit = document.getElementById('future-act-submit');
 
 pastbtn.onclick = function(){
     pastbtn.style.display = 'none';
@@ -16,14 +19,44 @@ pastbtn.onclick = function(){
 
 futurebtn.onclick = function(){
     futurebtn.style.display = 'none';
-    futureform.style.display = 'block';
+    futureform.style.display = 'flex';
+    futuresubmit.style.display = 'block'
+    futureactsubmit.style.display = 'none'
 };
 
+
 pastsubmit.onclick = function(){
+    let pastdate = document.getElementById('past-date');
+    pastdate = pastdate.value;
+    console.log(pastdate);
+
+    let pastact = document.getElementById('past-act');
+    pastact = pastact.value;
+    console.log(pastact);
+
+    let time = document.getElementById('time');
+    time = time.value;
+    console.log(time);
+
+    if (pastdate === '' || pastdate == null || pastact === '' || pastact == null || time === '' || time == null) {
+        window.alert('Invalid Post Activity. Please fill in the entire form.')
+        return;
+    }
+
+    pastactsubmit.textContent = "Got it!";
+
     pastform.style.display = 'none';
     pastsubmit.style.display = 'none';
-    pastactsubmit.textContent = "hello!\n";
     pastactsubmit.style.display = 'block';
     pastbtn.style.display = 'block';
+    pastform.reset();
+}
+
+futuresubmit.onclick = function(){
+    futureform.style.display = 'none';
+    futuresubmit.style.display = 'none';
+    futureactsubmit.textContent = "hello!\n";
+    futureactsubmit.style.display = 'block';
+    futurebtn.style.display = 'block';
 }
 
