9960a4144795a59235631099e688161d66b57019
JWK7 <76973860+JWK7@users.noreply.github.com>
2021-04-14 23:54:12 -0700

    added bold in message

--- public/examples.js
+++ public/examples.js
@@ -10,6 +10,9 @@ const futuresubmit = document.getElementById('future-submit');
 const pastactsubmit = document.getElementById('past-act-submit');
 const futureactsubmit = document.getElementById('future-act-submit');
 
+const pastbold = document.getElementById('past-bold');
+const futurebold = document.getElementById('future-bold');
+
 pastbtn.onclick = function(){
     pastbtn.style.display = 'none';
     pastform.style.display = 'flex';
@@ -39,39 +42,48 @@ pastact.addEventListener('change', (event) => {
 pastsubmit.onclick = function(){
     let pastdate = document.getElementById('past-date');
     pastdate = pastdate.value;
-    console.log(pastdate);
 
     let pastact = document.getElementById('past-act');
     pastact = pastact.value;
-    console.log(pastact);
 
     let time = document.getElementById('time');
     time = time.value;
-    console.log(time);
 
     let units = document.getElementById('units');
     units = units.value;
-    console.log(units);
 
     if (pastdate === '' || pastdate == null || pastact === '' || pastact == null || time === '' || time == null || units === '' || units == null) {
         window.alert('Invalid Post Activity. Please fill in the entire form.')
         return;
     }
 
-    pastactsubmit.textContent = `Got it! ${pastact} for ${time} ${units}. Keep it up!`;
+    pastbold.textContent = `${pastact} for ${time} ${units}.`;
 
     pastform.style.display = 'none';
     pastsubmit.style.display = 'none';
-    pastactsubmit.style.display = 'block';
+    pastactsubmit.style.display = 'flex';
     pastbtn.style.display = 'block';
     pastform.reset();
 }
 
 futuresubmit.onclick = function(){
+    let futuredate = document.getElementById('future-date');
+    futuredate = futuredate.value;
+
+    let futureact = document.getElementById('future-act');
+    futureact = futureact.value;
+
+    if (futuredate === '' || futuredate == null || futureact === '' || futureact == null) {
+        window.alert('Invalid Post Activity. Please fill in the entire form.')
+        return;
+    }
+
+    futurebold.textContent = `${futureact} on ${futuredate}`;
+
     futureform.style.display = 'none';
     futuresubmit.style.display = 'none';
-    futureactsubmit.textContent = "hello!\n";
-    futureactsubmit.style.display = 'block';
+    futureactsubmit.style.display = 'flex';
     futurebtn.style.display = 'block';
+    futureform.reset();
 }
 
